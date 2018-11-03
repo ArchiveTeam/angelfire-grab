00b2ae85c1fbd851cfeff4a24f5e7918d4e6a116
Adin Biederman <adinbied@gmail.com>
2018-11-03 11:17:47 -0700

    Get site items along with the index

--- angelfire.lua
+++ angelfire.lua
@@ -8,7 +8,7 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
       if string.match(l, "<loc>[^<]*</loc>") then
         l = string.gsub(l, "%s*<loc>", "")
         l = string.gsub(l, "</loc>", "")
-        table.insert(urls, {url=l})
+        table.insert(urls, {url=l, link_expect_html=1})
       end
     end
   end
