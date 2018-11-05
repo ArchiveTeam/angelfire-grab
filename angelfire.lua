f71729f8fbfaf48d2d1a190ec56a9de6331c1fd2
Adin Biederman <adinbied@gmail.com>
2018-11-05 09:33:42 -0800

    Re-wrote based on previous project and fixed recursive issue

--- angelfire.lua
+++ angelfire.lua
@@ -1,9 +1,64 @@
+local item_type = os.getenv('item_type')
+local item_value = os.getenv('item_value')
+local item_dir = os.getenv('item_dir')
+local warc_file_base = os.getenv('warc_file_base')
+
 local url_count = 0
 local tries = 0
+local downloaded = {}
+local addedtolist = {}
+local abortgrab = false
+
+read_file = function(file)
+  if file then
+    local f = assert(io.open(file))
+    local data = f:read("*all")
+    f:close()
+    return data
+  else
+    return ""
+  end
+end
+
+allowed = function(url, parenturl)
+  if string.match(url, "'+")
+     or string.match(url, "[<>\\]")
+     or string.match(url, "//$")
+     or string.match(url, "^https?://[^/]*facebook%.com")
+     or string.match(url, "^https?://[^/]*twitter%.com") then
+    return false
+  end
+  return false
+end
+
+wget.callbacks.download_child_p = function(urlpos, parent, depth, start_url_parsed, iri, verdict, reason)
+  local url = urlpos["url"]["url"]
+  local html = urlpos["link_expect_html"]
+
+  if string.match(url, "%[/") then
+    return false
+  end
+
+  -- Because the post-redirect URL is already in `downloaded`, this function would return false for it,
+  -- which causes wget to not retrieve (or even consider retrieving) any URLs found on the page unless
+  -- they're added explicitly in get_urls. It would therefore miss all page requisites, for example.
+  -- As a workaround, we let this function return true for a thread URL which has our made-up "x.ID"
+  -- URL as a parent (which should only happen for the initial redirects).
+  if (
+       (downloaded[url] ~= true and addedtolist[url] ~= true)
+       and (allowed(url, parent["url"]) or html == 0)
+     ) then
+    addedtolist[url] = true
+    return true
+  end
+
+  return false
+end
 
 wget.callbacks.get_urls = function(file, url, is_css, iri)
   local urls = {}
-  if string.match(url, "/sitemap.xml$") then
+  local html = nil
+ if string.match(url, "/sitemap.xml$") then
     for l in io.lines(file) do
       if string.match(l, "<loc>[^<]*</loc>") then
         l = string.gsub(l, "%s*<loc>", "")
@@ -12,6 +67,74 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
       end
     end
   end
+  downloaded[url] = true
+  
+  local function check(urla)
+    local origurl = url
+    local url = string.gsub(string.match(urla, "^([^#]+)"), "&amp;", "&")
+    if (downloaded[url] ~= true and addedtolist[url] ~= true)
+       and allowed(url, origurl) then
+      table.insert(urls, { url=url })
+      addedtolist[origurl] = true
+      addedtolist[url] = true
+    end
+  end
+
+  local function checknewurl(newurl)
+    if string.match(newurl, "^https?:////") then
+      check(string.gsub(newurl, ":////", "://"))
+    elseif string.match(newurl, "^https?://") then
+      check(newurl)
+    elseif string.match(newurl, "^https?:\\/\\?/") then
+      check(string.gsub(newurl, "\\", ""))
+    elseif string.match(newurl, "^\\/\\/") then
+      check(string.match(url, "^(https?:)")..string.gsub(newurl, "\\", ""))
+    elseif string.match(newurl, "^//") then
+      check(string.match(url, "^(https?:)")..newurl)
+    elseif string.match(newurl, "^\\/") then
+      check(string.match(url, "^(https?://[^/]+)")..string.gsub(newurl, "\\", ""))
+    elseif string.match(newurl, "^/") then
+      check(string.match(url, "^(https?://[^/]+)")..newurl)
+    end
+  end
+
+  local function checknewshorturl(newurl)
+    if string.match(newurl, "^%?") then
+      check(string.match(url, "^(https?://[^%?]+)")..newurl)
+    elseif not (string.match(newurl, "^https?:\\?/\\?//?/?")
+       or string.match(newurl, "^[/\\]")
+       or string.match(newurl, "^[jJ]ava[sS]cript:")
+       or string.match(newurl, "^[mM]ail[tT]o:")
+       or string.match(newurl, "^vine:")
+       or string.match(newurl, "^android%-app:")
+       or string.match(newurl, "^%${")) then
+      check(string.match(url, "^(https?://.+/)")..newurl)
+    end
+  end
+  
+  if allowed(url, nil) then
+    html = read_file(file)
+
+    for newurl in string.gmatch(html, '([^"]+)') do
+      checknewurl(newurl)
+    end
+    for newurl in string.gmatch(html, "([^']+)") do
+      checknewurl(newurl)
+    end
+    for newurl in string.gmatch(html, ">%s*([^<%s]+)") do
+      checknewurl(newurl)
+    end
+    for newurl in string.gmatch(html, "[^%-]href='([^']+)'") do
+      checknewshorturl(newurl)
+    end
+    for newurl in string.gmatch(html, '[^%-]href="([^"]+)"') do
+      checknewshorturl(newurl)
+    end
+    for newurl in string.gmatch(html, ":%s*url%(([^%)]+)%)") do
+      check(newurl)
+    end
+  end
+
   return urls
 end
 
@@ -22,27 +145,32 @@ wget.callbacks.httploop_result = function(url, err, http_stat)
   io.stdout:write(url_count .. "=" .. status_code .. " " .. url["url"] .. "  \n")
   io.stdout:flush()
 
-  if status_code == 401 then
+  if (status_code >= 200 and status_code <= 399) then
+    downloaded[url["url"]] = true
+    downloaded[string.gsub(url["url"], "https?://", "http://")] = true
+  end
+
+  if abortgrab == true then
+    io.stdout:write("ABORTING...\n")
     return wget.actions.ABORT
   end
   
   if status_code >= 500 or
-    (status_code >= 400 and status_code ~= 403 and status_code ~= 404 and status_code ~= 410) or
+    (status_code >= 400 and status_code ~= 404) or
     status_code == 0 then
     io.stdout:write("Server returned "..http_stat.statcode.." ("..err.."). Sleeping.\n")
     io.stdout:flush()
-    if status_code == 429 then
-      os.execute("sleep 300")
-      return wget.actions.ABORT
-    else
-      os.execute("sleep 1")
-    end
+    os.execute("sleep 1")
     tries = tries + 1
     if tries >= 5 then
       io.stdout:write("\nI give up...\n")
       io.stdout:flush()
       tries = 0
-      return wget.actions.ABORT
+      if allowed(url["url"], nil) then
+        return wget.actions.ABORT
+      else
+        return wget.actions.EXIT
+      end
     else
       return wget.actions.CONTINUE
     end
@@ -59,3 +187,9 @@ wget.callbacks.httploop_result = function(url, err, http_stat)
   return wget.actions.NOTHING
 end
 
+wget.callbacks.before_exit = function(exit_status, exit_status_string)
+  if abortgrab == true then
+    return wget.exits.IO_FAIL
+  end
+  return exit_status
+end
